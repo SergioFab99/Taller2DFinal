0a62ecc8bfea634df4619d1c957110fd0460251e
Christian Norabuena <cnorabuena5@gmail.com>
2023-12-08 17:01:31 -0500

    Uso de jeringa - invulnerabilidad 3 segundos

---
+++ Assets/Scripts/Player/Jeringa_Main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Jeringa_Main : MonoBehaviour
+{
+    static void Main(string[] args)
+    {
+        Player myPlayer = new Player();
+
+        myPlayer.UsarObjeto();
+
+
+        bool Invulnerableahora = myPlayer.EstaInvulnerable();
+        Debug.Log("¿Está Invulnerable?" + Invulnerableahora);
+
+    }
+}
---
+++ Assets/Scripts/Player/Jeringa_Use.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Jeringa_Use : MonoBehaviour
+{
+    [SerializeField] private bool invulnerable;
+
+    private void Start()
+    {
+        invulnerable = false;
+    }
+
+    public void UsarObjeto()
+    {
+        StartCoroutine(ActivarInvulnerabilidad());
+    }
+
+    private IEnumerator ActivarInvulnerabilidad()
+    {
+        Debug.Log("Usando Jeringa - activar invulnerabilidad");
+        invulnerable = true;
+
+        yield return new WaitForSeconds(3); // Espera de 3 segundos
+
+        invulnerable = false;
+        Debug.Log("Terminar invulnerabilidad");
+    }
+
+    public bool EstaInvulnerable()
+    {
+        return invulnerable;
+    }
+}
