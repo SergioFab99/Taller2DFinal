83a565e4c5ed5580309459793f78a64aa6138b5d
NicollDeColl <nicolldecoll2102@gmail.com>
2023-12-08 22:49:16 -0500

    AGREGUE script para textos y audios (faltan cambios)

---
+++ Assets/Scripts/Game/UI/ControllerText.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+
+
+public class ControladorTexto : MonoBehaviour
+{
+  /*  public TextMeshProUGUI textoUI;
+    public GameObject playerColision;
+    public AudioSource sonido;
+
+    void Start()
+    {
+        if (textoUI == null)
+        {
+            Debug.LogError("El objeto TextMeshProUGUI no está asignado en el Inspector.");
+        }
+        else
+        {
+            MostrarTexto("");
+        }
+
+        if (sonido == null)
+        {
+            Debug.LogError("El objeto AudioSource no está asignado en el Inspector.");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (playerColision.CompareTag("ActivarTxt1"))
+        {
+                StartCoroutine(CambiarTextoDespuesDeEspera());
+                if (sonido != null)
+                {
+                    sonido.Play();  
+                } 
+        }
+    }
+
+    IEnumerator CambiarTextoDespuesDeEspera()
+    {
+
+        MostrarTexto("Seishu, hola, soy tu maestra de quimica"); 
+        yield return new WaitForSeconds(5f);
+        MostrarTexto("Como veras, ha pasado algo muy extraño...");
+        yield return new WaitForSeconds(5f);
+        MostrarTexto("Necesitamos tu ayuda, varios de tus compañeros y yo estamos encerrados abajo del colegio,");
+        yield return new WaitForSeconds(5f);
+        MostrarTexto("Tienes que encontrar la entrada y rescatarnos, por favor");
+    }
+
+    void MostrarTexto(string mensaje)
+    {
+        textoUI.text = mensaje;
+    } */
+}
+
+  
