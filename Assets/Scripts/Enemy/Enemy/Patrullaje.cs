8c0e38aaf74d3e0f2a0b2c8f4ba4bcd2ebeaacf9
Remaining22 <128372243+Remaining22@users.noreply.github.com>
2023-12-09 14:38:52 -0500

    Patrullaje y deteccion del enemigo para seguimiento del jugador

--- Assets/Scripts/Enemy/Enemy/Patrullaje.cs
+++ Assets/Scripts/Enemy/Enemy/Patrullaje.cs
@@ -1,18 +1,74 @@
+using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Patrullaje : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Transform currentTarget;
+    private GameObject[] targetList;
+    private GameObject Player;
+    private AIDestinationSetter targetSetter;
+    [SerializeField] private bool playerDetected = false;
+    RaycastHit2D Hit;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] LayerMask LayersHit;
+    [SerializeField] float AggroDistance = 25;
+    private void Awake()
     {
-        
+        targetList = GameObject.FindGameObjectsWithTag("Patrullaje");
+        targetSetter = GetComponent<AIDestinationSetter>();
+        Player = GameObject.Find("Player");
+        GetRandomTarget();
+        UpdateTarget();
+    }
+    private void Update()
+    {
+        checkForPlayer();
+        if (targetList != null)
+        {
+            if (IsDoneWithTarget() && !playerDetected)
+            {
+                GetRandomTarget();
+            }
+        }
+        UpdateTarget();
+    }
+    bool IsDoneWithTarget()
+    {
+        if(Vector2.Distance(transform.position, currentTarget.position) < 0.5)
+        {
+            return true;
+        }
+        else 
+        {
+            return false; 
+        }
+    }
+    void GetRandomTarget()
+    {
+        int random = UnityEngine.Random.Range(0, targetList.Length);
+        currentTarget = targetList[random].transform;
+    }
+    void UpdateTarget()
+    {
+        targetSetter.target = currentTarget;
+        if (playerDetected)
+        {
+            targetSetter.target = Player.transform;
+        }
+    }
+    void checkForPlayer()
+    {
+        Vector2 Direction = Player.transform.position - transform.position;
+        Hit = Physics2D.Raycast(transform.position, Direction.normalized, AggroDistance, LayersHit);
+        Debug.DrawRay(transform.position, Direction);
+        if(Hit.collider != null)
+        {
+            if (Hit.collider.gameObject.CompareTag("Player"))
+            {
+                playerDetected = true;
+            }
+        }
     }
 }
