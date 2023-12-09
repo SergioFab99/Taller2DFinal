3889ad08d1bb38124cafaaf9d4eeb1bce859dae7
NicollDeColl <nicolldecoll2102@gmail.com>
2023-12-09 01:22:59 -0500

    Script para reproducir sonido y que aparezca el texto

--- Assets/Scripts/Game/UI/ControllerText.cs
+++ Assets/Scripts/Game/UI/ControllerText.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class ControladorTexto : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class ControladorTexto : MonoBehaviour
     public AudioSource sonido;
     public bool activar;
     public AudioClip clip1;
+    public BoxCollider2D col;
 
     void Start()
     {
@@ -31,17 +33,26 @@ public class ControladorTexto : MonoBehaviour
         }
     }
 
-       IEnumerator CambiarTextoDespuesDeEspera()
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            (col).enabled = false;
+        }
+    }
+
+    IEnumerator CambiarTextoDespuesDeEspera()
     {
         sonido.PlayOneShot(clip1);
         TextoFuncion("Seishu, hola, soy tu maestra de quimica");
         yield return new WaitForSeconds(5f);
-        TextoFuncion("Como veras, ha pasado algo muy extraño...");
+        TextoFuncion("Como veras, ha pasado algo fuera de lo normal...");
         yield return new WaitForSeconds(5f);
-        TextoFuncion("Necesitamos tu ayuda, varios de tus compañeros y yo estamos encerrados abajo del colegio,");
+        TextoFuncion("Necesitamos tu ayuda, algunos de tus amigos y yo estamos encerrados abajo del colegio ");
         yield return new WaitForSeconds(5f);
         TextoFuncion("Tienes que encontrar la entrada y rescatarnos, por favor");
         yield return new WaitForSeconds(5f);
+        TextoFuncion(" ");
         sonido.Stop();
     }
     void TextoFuncion(string Texto)
