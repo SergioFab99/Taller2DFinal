1ca78793118211034e207a2995ea17f28aa5c3db
NicollDeColl <nicolldecoll2102@gmail.com>
2023-12-12 19:38:29 -0500

    Script para abrir las puertas de los casilleros implementado

---
+++ Assets/Scripts/Game/CasilleroE.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasilleroE : MonoBehaviour
+{
+    public GameObject letra;
+    public Animator anim;
+    public bool dentro;
+    public bool abierto;
+
+
+    void Update()
+    {
+       if(dentro)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetBool("Presionado", true);
+                abierto = true;
+            }
+        }
+        
+       if(abierto)
+        {
+            letra.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            letra.SetActive(true);
+            dentro = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.SetBool("Presionado", false);
+
+            letra.SetActive(false);
+        }
+    }
+}
